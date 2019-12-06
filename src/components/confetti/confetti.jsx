f85db8b82ad6085fcdaa9b0282c32dad342027c0
Sergey Lem <sergey.lem@gmail.com>
2019-12-07 00:28:24 +0600

    initialization

--- src/components/confetti/confetti.jsx
+++ src/components/confetti/confetti.jsx
@@ -12,3 +12,17 @@ export default (props) => {
     />
   )
 }
+
+// import React from 'react'
+// import useWindowSize from 'react-use/lib/useWindowSize'
+// import Confetti from 'react-confetti'
+ 
+// export default () => {
+//   const { width, height } = useWindowSize()
+//   return (
+//     <Confetti
+//       width={width}
+//       height={height}
+//     />
+//   )
+// }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'tachyons';
+//import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
