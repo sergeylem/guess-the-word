78589615e59d7243049941dbd93676f7651cc20f
Sergey Lem <sergey.lem@gmail.com>
2020-07-07 23:22:13 +0600

    Refactoring

--- src/components/play-sound.js
+++ src/components/play-sound.js
@@ -3,12 +3,12 @@ import Sound from 'react-sound';
 
 const PlaySound = (props) => {
   return (
-    <Sound  //Sound must be in the same string where is RETURN or using ()
-      url = {props.urlStr}
-      autoLoad = {true}
-      playStatus = {Sound.status.PLAYING} 
-    />   
-  )     
+    <Sound
+      url={props.urlStr}
+      autoLoad={true}
+      playStatus={Sound.status.PLAYING}
+    />
+  )
 };
 
 export default PlaySound;
