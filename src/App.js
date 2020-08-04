2d4d879175e648ffb84bd3f545369a09a75dcb38
Sergey Lem <sergey.lem@gmail.com>
2020-08-04 23:52:46 +0600

    Refactoring

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Confetti from './components/confetti/confetti';
 import PlaySound from './components/play-sound';
 
 const App = () => {
-  
+
   const [words, setWords] = useState(data.words);
   const [targetLetters, setTargetLetters] = useState(data.targetLetters);
   const [userLetters, setUserLetters] = useState(data.userLetters);
@@ -24,25 +24,25 @@ const App = () => {
   const [viewportHeight, setViewportHeight] = useState(data.viewportHeight);
 
   useEffect(() => initialState(), []);
-  
+
   const initialState = () => {
-    //    console.log(this.state.words);
+    //    console.log(words);
 
     const index = getRndNumber(0, maxCountWords); //index of random array of words
-    const targetLetters = [...words[index].word];
-    const targetWordId = words[index].id;
-    const userLetters = putUnderscores(targetLetters.length);
+    const _targetLetters = [...words[index].word];
+    const _targetWordId = words[index].id;
+    const _userLetters = putUnderscores(_targetLetters.length);
     const countLettersToAdd = 0; //Math.floor(targetLetters.length / 3);
-    const rndLetters = getRndLetters(targetLetters, countLettersToAdd);
+    const _rndLetters = getRndLetters(_targetLetters, countLettersToAdd);
 
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
-    
-    setTargetWordId(targetWordId);
+
     setTargetWordIndex(index);
-    setTargetLetters([...words[index].word]);
-    setUserLetters(userLetters);
-    setRndLetters(rndLetters);
+    setTargetWordId(_targetWordId);
+    setTargetLetters(_targetLetters);
+    setUserLetters(_userLetters);
+    setRndLetters(_rndLetters);
     setViewportWidth(viewportWidth);
     setViewportHeight(viewportHeight);
 
@@ -52,142 +52,160 @@ const App = () => {
     // });
   }
 
-  const addLetter = (newLetter, userLetters, rndLetters, index) => {
-    const underscore = ' ';
-    for (let i = 0; i < userLetters.length; i++) {
-      if (userLetters[i] === underscore) {
-        userLetters[i] = newLetter;
-        rndLetters[index] = ' ';
-        i = userLetters.length; //остановка цикла
+  const isTheWordGuessed = () => {
+    for (let i = 0; i < targetLetters.length; i++) {
+      if (userLetters[i] !== targetLetters[i]) {
+        return false;
       }
     }
-    const isGuessed = isTheWordGuessed(userLetters);
-    let isGameOver = data.isGameOver;
-    if (data.words.length === 1) {
-      isGameOver = true;
-    }
-
-    //    console.log(userLetters);
-    //    console.log('isGuessed from addLetter ' + isGuessed);
-
-    if (isGuessed) {
-      this.setState({ isConfetti: true });
-      setTimeout(() => this.setState({ isGuessed, isGameOver, isConfetti: false, isFade: true }), 3000);
-    } else
-      this.setState({ isGuessed });
+    return true; //The word has been guessed!
   }
 
-  return (<div>TEST</div>);
+  const addLetter = (newLetter, index) => {
+    const underscore = ' ';
+    const _userLetters = userLetters;
+    const _rndLetters = rndLetters;
+    for (let i = 0; i < _userLetters.length; i++) {
+      if (_userLetters[i] === underscore) {
+        _userLetters[i] = newLetter;
+        _rndLetters[index] = ' ';
+        i = _userLetters.length; //остановка цикла
+      }
+    }
+    const _isGuessed = isTheWordGuessed(_userLetters);
 
-  // const delLetter = (letter, userLetters, rndLetters, index) => {
-  //   for (let i = 0; i < rndLetters.length; i++) {
-  //     if (rndLetters[i] === ' ') {
-  //       rndLetters[i] = letter;
-  //       userLetters[index] = ' ';
-  //       i = rndLetters.length; //остановка цикла
-  //     }
-  //   }
-  //   this.setState({ userLetters, rndLetters });
-  // }
+    let _isGameOver = isGameOver;
 
-  // const isTheWordGuessed = (userLetters) => {
-  //   for (let i = 0; i < data.targetLetters.length; i++) {
-  //     if (userLetters[i] !== data.targetLetters[i]) {
-  //       return false;
-  //     }
-  //   }
-  //   return true; //The word has been guessed!
-  // }
+    if (words.length === 1) {
+      _isGameOver = true;
+    }
 
-  // const removeItemFromWords = () => {
-  //   //    console.log('words ');
-  //   //    console.log(this.state.words);
+    console.log("_userLetters: " + _userLetters);
+    console.log('_isGuessed1 from addLetter ' + _isGuessed);
 
-  //   const removedId = data.targetWordId;
-  //   let tmpArray = data.words;  //words записываю во временный массив 
-  //   const removedIndex = tmpArray.findIndex(idWord => idWord.id === removedId);
+    setUserLetters(_userLetters);
+    setRndLetters(_rndLetters);
 
-  //   tmpArray.splice(removedIndex, 1);  //Удаляю word из words
+    console.log("userLetters: " + userLetters);
+    console.log('_isGuessed2 from addLetter ' + _isGuessed);
 
-  //   const maxCountWords = data.maxCountWords - 1;
-  //   if (maxCountWords >= 0) {
-  //     const newIndex = getRndNumber(0, maxCountWords);
-  //     const newTargetLetters = [...tmpArray[newIndex].word];
-  //     const newWordId = tmpArray[newIndex].id;
+    if (_isGuessed) {
+      setConfetti(true);
+      setTimeout(() => { setGuessed(_isGuessed); setGameOver(_isGameOver); setConfetti(false); setFade(true); }, 3000);
+    } else
+      setGuessed(_isGuessed);
+  }
 
-  //     const userLetters = putUnderscores(newTargetLetters.length);
-  //     const countLettersToAdd = 0; //Math.floor(newTargetLetters.length / 3);
+  const delLetter = (letter, index) => {
+    for (let i = 0; i < rndLetters.length; i++) {
+      if (rndLetters[i] === ' ') {
+        rndLetters[i] = letter;
+        userLetters[index] = ' ';
+        i = rndLetters.length; //остановка цикла
+      }
+    }
+    setUserLetters(userLetters);
+    setRndLetters(rndLetters);
+  }
 
-  //     const targetLetters = [...newTargetLetters]; //it's must be, as targetLetters will be changed
-  //     const rndLetters = getRndLetters(targetLetters, countLettersToAdd)
-  //     //      console.log('rndLetters: ' + rndLetters);
+  const removeItemFromWords = () => {
+    //    console.log('words ');
+    //    console.log(words);
 
-  //     this.setState({
-  //       words: tmpArray,
-  //       targetLetters: newTargetLetters, targetWordId: newWordId, targetWordIndex: newIndex,
-  //       userLetters, rndLetters, maxCountWords, isGuessed: false, isFade: true
-  //     });
-  //   }
-  // }
+    const removedId = targetWordId;
+    let tmpArray = words;  //words записываю во временный массив 
+    const removedIndex = tmpArray.findIndex(idWord => idWord.id === removedId);
+
+    tmpArray.splice(removedIndex, 1);  //Удаляю word из words
+
+    //    const maxCountWords = maxCountWords - 1;
+    setMaxCountWords(maxCountWords - 1);
+    if (maxCountWords >= 0) {
+      const newIndex = getRndNumber(0, maxCountWords);
+      const newTargetLetters = [...tmpArray[newIndex].word];
+      const newWordId = tmpArray[newIndex].id;
+
+      // const userLetters = putUnderscores(newTargetLetters.length);
+      const userLetters = putUnderscores(newTargetLetters.length);
+      const countLettersToAdd = 0; //Math.floor(newTargetLetters.length / 3);
+
+      const targetLetters = [...newTargetLetters]; //it's must be, as targetLetters will be changed
+      const rndLetters = getRndLetters(targetLetters, countLettersToAdd)
+      //      console.log('rndLetters: ' + rndLetters);
 
 
-  // const isFade = data.isFade
-  // return (
-  //   <div className={isFade ? 'form-fade-animation' : 'form'}
-  //   >
+      setWords(tmpArray);
+      setTargetLetters(newTargetLetters);
+      setTargetWordId(newWordId);
+      setTargetWordIndex(newIndex);
+      setUserLetters(userLetters);
+      setRndLetters(rndLetters);
+      setMaxCountWords(maxCountWords);
+      setGuessed(false);
+      setFade(true);
 
-  //     <div className='title'>
-  //       {!data.isGameOver ?
-  //         "Guess the word!" :
-  //         "Well done! Congratulations!"
-  //       }
-  //     </div>
+      // this.setState({
+      //   words: tmpArray,
+      //   targetLetters: newTargetLetters, targetWordId: newWordId, targetWordIndex: newIndex,
+      //   userLetters, rndLetters, maxCountWords, isGuessed: false, isFade: true
+      // });
+    }
+  }
 
-  //     <img className='image'
-  //       src={data.words[data.targetWordIndex].img} alt='' />
+  return (
+    <div className={isFade ? 'form-fade-animation' : 'form'}
+    >
 
-  //     {!data.isGuessed ?
-  //       <div>
-  //         <div className='words'>
-  //           {data.userLetters.map((item, index) => (
-  //             <Button letter={item} key={index}
-  //               click={delLetter.bind(
-  //                 this, item, data.userLetters, data.rndLetters, index)} />
-  //           ))}
-  //         </div>
+      <div className='title'>
+        {!isGameOver ?
+          "Guess the word!" :
+          "Well done! Congratulations!"
+        }
+      </div>
 
-  //         <div className='words'>
-  //           {data.rndLetters.map((item, index) => (
-  //             <Button letter={item} key={index}
-  //               click={addLetter.bind(
-  //                 this, item, data.userLetters, data.rndLetters, index)} />
-  //           ))}
-  //         </div>
-  //         {data.isConfetti ?
-  //           <div>
-  //             <Confetti viewportWidth={data.viewportWidth}
-  //               viewportHeight={data.viewportHeight}
-  //               numberOfPieces={'100'} />
-  //             <PlaySound urlStr={require('./assets/sounds/s2.mp3')} />
-  //           </div> :
-  //           null
-  //         }
-  //       </div> :
-  //       <div>
-  //         <div className='word'>{data.targetLetters}</div>
-  //         {!data.isGameOver
-  //           ? <img
-  //             className={'arrow-next'}
-  //             src={require('./assets/icons/arrow-next.png')} alt=''
-  //             onClick={removeItemFromWords.bind(this)}
-  //           />
-  //           :
-  //           <PlaySound urlStr={require('./assets/sounds/victory.mp3')} />
-  //         }
-  //       </div>
-  //     }
-  //   </div>
-  // );
+      <img className='image'
+        src={words[targetWordIndex].img} alt='' />
+
+      {!isGuessed ?
+        <div>
+          <div className='words'>
+            {userLetters.map((item, index) => (
+              <Button letter={item} key={index}
+                click={() => delLetter(item, index)} />
+            ))}
+          </div>
+
+          <div className='words'>
+            {rndLetters.map((item, index) => (
+              <Button letter={item} key={index}
+                click={() => addLetter(item, index)} />
+            ))}
+          </div>
+          {isConfetti ?
+            <div>
+              <Confetti viewportWidth={viewportWidth}
+                viewportHeight={viewportHeight}
+                numberOfPieces={'100'} />
+              <PlaySound urlStr={require('./assets/sounds/s2.mp3')} />
+            </div> :
+            null
+          }
+        </div> :
+        <div>
+          <div className='word'>{targetLetters}</div>
+          {!isGameOver
+            ? <img
+              className={'arrow-next'}
+              src={require('./assets/icons/arrow-next.png')} alt=''
+              onClick={() => removeItemFromWords()}
+            />
+            :
+            <PlaySound urlStr={require('./assets/sounds/victory.mp3')} />
+          }
+        </div>
+      }
+    </div>
+  );
 }
 
 export default App;
